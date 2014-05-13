dc2182750f2961dca496e43010f8cef9a9442e9a
Matt Brandt <mbrandt@mozilla.com>
2014-05-12 21:12:44 -0600

    cleanup + pep8

--- sensors.py
+++ sensors.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import datetime
 from ds18b20 import DS18B20
 
+
 sensor = DS18B20()
 
+
 def get_time():
-   now = datetime.datetime.now()
-   return now.strftime("%Y-%m-%d %H:%M")
+    now = datetime.datetime.now()
+    return now.strftime("%Y-%m-%d %H:%M")
+
 
 def get_kelvin():
     return sensor.get_temperatures([DS18B20.KELVIN])
 
+
 def get_celsius():
     return sensor.get_temperatures([DS18B20.DEGREES_C])
+
 
 def get_fahrenheit():
     return sensor.get_temperatures([DS18B20.DEGREES_F])
--- weather.py
+++ weather.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 from flask import jsonify, Flask, render_template
 import sensors
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-   templateData = {
-      'title': 'Temperature :: 34 Orchard Lane',
-      'time': sensors.get_time(),
-      'kelvin': sensors.get_kelvin(),
-      'celsius': sensors.get_celsius(),
-      'fahrenheit': sensors.get_fahrenheit()
-      }
-   return render_template('main.html', **templateData)
+    templateData = {
+        'title': 'Temperature :: Home',
+        'time': sensors.get_time(),
+        'kelvin': sensors.get_kelvin(),
+        'celsius': sensors.get_celsius(),
+        'fahrenheit': sensors.get_fahrenheit()
+    }
+    return render_template('main.html', **templateData)
 
 weather_data = [
     {
@@ -26,9 +32,11 @@ weather_data = [
     }
 ]
 
-@app.route('/weather/api/v1.0/temperature', methods = ['GET'])
+
+@app.route('/weather/api/v1.0/temperature', methods=['GET'])
 def get_temp():
-   return jsonify( { 'weather_data': weather_data } )
+    return jsonify({'weather_data': weather_data})
+
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=4444, debug=True)
+    app.run(host='0.0.0.0', port=4444, debug=True)

1f81c127b1760c698dddb15ae73877f0d27a588f
m8ttyb <mbrandt@mozilla.com>
2014-05-14 21:24:20 +0000

    javascript is terribly difficult for me

--- static/script.js
+++ static/script.js
@@ -3,10 +3,8 @@ $(document).ready(function() {
       url: "http://10.0.1.137:4444/weather/api/v1.0/temperature",
       contentType: "application/json",
       dataType: "json",
-      success: function(data){ 
-          console.log(data);
-          $('#fahrenheit').append(JSON.stringify(obj.weather_data[0].fahrenheit));
-          $('#fahrenheit').append(String(data.weather_data));
+      success: function(response){ 
+          $('#fahrenheit').append(response.weather_data[0].fahrenheit[0]);
       },
       error: function (data, jqXHR, textStatus, errorThrown) {
           $("#error").append("<h3>Error message</h3>jqXHR: " + jqXHR.status + "\ntextStatus: " + textStatus + "\nerrorThrown: " + errorThrown);
